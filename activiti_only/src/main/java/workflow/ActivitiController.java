301faff2f9ca7372fe9b1f47a93b65a5c0b1b692
tengtt <1055954019@qq.com>
2016-09-08 18:12:51 +0800

    activiti

--- activiti_only/src/main/java/workflow/ActivitiController.java
+++ activiti_only/src/main/java/workflow/ActivitiController.java
@@ -1,17 +1,20 @@
 package workflow;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by teng on 2016/7/28.
+ * 测试
  */
 
 @Controller
 @RequestMapping(value = "/workflow")
 public class ActivitiController {
 
-    protected Logger logger = LoggerFactory.getLogger(getClass());
+//    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    public void main(String[] args){
+        System.out.print(true);
+    }
 }
